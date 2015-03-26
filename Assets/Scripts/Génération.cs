5be42a1a8008aa94c4158b3ccb7e77fd8e0511ab
nosuke <florent.lucet.1@ulaval.ca>
2015-03-26 05:38:48 +0100

    Ajout du projet.

---
+++ Assets/Scripts/Génération.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Génération : MonoBehaviour {
+
+	public GameObject Obstacle;
+	private int score = 0;
+
+	void Start() {
+		InvokeRepeating("CréerObstacle", 1f, 1.5f);
+	}
+
+	void OnGUI () 
+	{
+		GUI.color = Color.red;
+		GUILayout.Label(" Score : " + score.ToString());
+	}
+
+	void CréerObstacle() {
+		Instantiate(Obstacle);
+		score++;
+	}
+}
---
+++ Assets/Scripts/Joueur.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Joueur : MonoBehaviour {
+
+	public Vector2 jumpForce = new Vector2(0, 300);
+
+	void Update () {
+		if (Input.GetKeyUp("space")) {
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForce);
+		}
+
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (screenPosition.y > Screen.height || screenPosition.y < 0) {
+			Mourir();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		Mourir();
+	}
+		
+	void Mourir() {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
